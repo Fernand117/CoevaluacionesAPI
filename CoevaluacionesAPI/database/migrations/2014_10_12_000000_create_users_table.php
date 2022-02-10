f99f50383a6644eeaa29df366588347b3b646f51
Pandolfo45 <80552473+Pandolfo45@users.noreply.github.com>
2022-02-10 12:59:08 -0600

    Migracion Usuario
    Configurando migraciones

--- CoevaluacionesAPI/database/migrations/2014_10_12_000000_create_users_table.php
+++ CoevaluacionesAPI/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('paterno');
+            $table->string('materno');
+            $table->boolean('estatus');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
