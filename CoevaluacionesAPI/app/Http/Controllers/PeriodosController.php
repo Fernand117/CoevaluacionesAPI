04d09c26d74bde3082dac0d36e3e972f1079dd44
Andrea SH <20183l101046@utcv.edu.mx>
2022-02-18 15:01:55 -0600

    Creación de periodos
    crup periodos

--- CoevaluacionesAPI/app/Http/Controllers/PeriodosController.php
+++ CoevaluacionesAPI/app/Http/Controllers/PeriodosController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PeriodosModel;
+
 
 class PeriodosController extends Controller
 {
-    //
+   public function listarPeriodos()
+   {
+       $periodos = PeriodosModel::all();
+       return response()->json(['status' => 200, 'Mensaje' => 'Listar periodos', 'Datos' =>$periodos], 200);
+   }
+   public function crearPeriodos(Request $request)
+    {
+        $datos = $request->all();
+        $periodos = new PeridosModel();
+
+        $peridos->periodos = $datos['peridos'];
+        $periodos->estado = '1';
+        $periodos->save();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'Periodo Generado ']);
+    }
+    public function editarPeriodos(Request $request)
+    {
+        $datos = $request->all();
+        $periodos = PeriodosModel::find($datos['idPeriodo']);
+
+        $periodo->periodo = $datos['periodo'];
+        $periodos->update();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'Periodo actualizado ']);
+    }
+    public function eliminarPeriodos(Request $request)
+    {
+        $datos = $request->all();
+        $peridos = PeriodosModel::find($datos['idGrado']);
+        $periodos->estado = '0';
+        $peridos->update();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'Periodo eliminado']);
+    }
 }
