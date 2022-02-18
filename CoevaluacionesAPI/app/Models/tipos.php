8a39b73f7f401334c48918a2b2d56fe099c7de0c
Pandolfo45 <80552473+Pandolfo45@users.noreply.github.com>
2022-02-18 01:13:30 -0600

    CRUD TIPOS_USUARIOS
    controlador modelos y migracion de tipos de usuarios

---
+++ CoevaluacionesAPI/app/Models/tipos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tipos extends Model
+{
+    use HasFactory;
+    protected $table="tipos";
+    protected $fillable=[
+        'tipos_usuarios'
+    ];
+}
