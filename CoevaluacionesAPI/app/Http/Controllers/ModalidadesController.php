06e28684d76aa1c5596033c08a9900367947bb1e
chino0rt24 <67172144+chino0rt24@users.noreply.github.com>
2022-02-18 00:51:26 -0600

    crontoller
    Creacion Controller Modalidades

---
+++ CoevaluacionesAPI/app/Http/Controllers/ModalidadesController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ModalidadesModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use SebastianBergmann\ObjectEnumerator\Exception;
+
+
+class ModalidadesController extends Controller
+{
+    //
+    
+
+         public function index(){
+        $data = ModalidadesModel::where('estatus','=',1)->get();
+        $Modalidades= [];
+        foreach($data as $key => $value){
+           $Modalidades[$key] = [
+                'id'=>$value['id'],
+                'Modalidad'=>$value['Modalidad'],
+                'clave'=>$value['clave'],
+                'Estatdo'=>$value['Estado'],
+            ];
+           
+         } 
+         return response()->json($Modalidades);
+
+
+    }
+    public function store(Request $request){
+        $datos = $request->all();
+        DB::beginTransaction();
+        try {
+            $Modalidades = new ModalidadesModel();
+            $Modalidades->Modalidad =  $datos['Modalidad'];
+            $Modalidades->Clave = $datos['Clave'];
+            $Modalidades->Estado = $datos['Estado'];
+            $Modalidades->save();
+
+            DB::commit();
+            return response()->json(array('success' => true));
+        }catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(array('success' => false));
+        }
+    }
+    public function edit($id)
+    {
+        //
+        $otraVar = ModalidadesModel::find($id);
+        
+        $masvar = [
+            'id'=>$otraVar['id'],
+            'Modalidad'=>$otraVar['Modalidad'],
+            'Clave'=>$otraVar['Clave'],
+            'Estado'=>$otraVar['Estado'],
+        ];
+        return response()->json($masvar);        
+    }
+    public function update(Request $request, $id)
+    {
+        //
+        $datos = $request->all();
+        DB::beginTransaction();
+        try {
+            $Modalidades = ModalidadesModel::find($id);
+            $Modalidades->Modalidad =  $datos['Modalidad'];
+            $Modalidades->Clave =  $datos['Clave'];
+            $Modalidades->Estado =  $datos['Estado'];
+            $Modalidades->update();
+
+            DB::commit();
+            return response()->json(array('success' => true));
+        }catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(array('success' => false));
+        }
+        
+
+}
+public function destroy($id)
+    {
+        
+        $Modalidades = ModalidadesModel::find($id);
+        $Modalidades->estatus=0;
+        $Modalidades->update();
+        return response()->json(array('success' => true));
+    }
+
+}
