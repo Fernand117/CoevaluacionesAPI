cfd865f9d6f1d42c68df00ee005f85837e3b6d5a
vags2022 <20183h101231@utcv.edu.mx>
2022-02-18 15:04:46 -0600

    Falta importar el modelo
    Crud de carreras solo falta importar los modelos

--- CoevaluacionesAPI/app/Http/Controllers/CarrerasController.php
+++ CoevaluacionesAPI/app/Http/Controllers/CarrerasController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 class CarrerasController extends Controller
 {
-    //
+ //
+    public function listarCarreras()
+    {
+        $grados = CarrerasModel::all();
+        return response()->json(['Status' => 200, 'Mensaje' => 'Lista de carreras', 'Datos' => $Carreras], 200);
+    }
+
+    public function crearGrupo(Request $request)
+    {
+        $datos = $request->all();
+        $grado = new CarrerasModel();
+
+        $grado->grado = $datos['carerras'];
+        $grado->estado = '1';
+        $grado->save();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'Carrera generado correctamente']);
+    }
+
+    public function editarCarrera(Request $request)
+    {
+        $datos = $request->all();
+        $grado = CarreraModel::find($datos['idCarrera']);
+
+        $grado->grado = $datos['carrera'];
+        $grado->update();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'actualizado correctamente']);
+    }
+
+    public function eliminarCarrera(Request $request)
+    {
+        $datos = $request->all();
+        $grado = CarreraModel::find($datos['idCarrera']);
+        $grado->estado = '0';
+        $grado->update();
+
+        return response()->json(['Status' => 200, 'Mensaje' => 'eliminado correctamente']);
+
+    }
 }
