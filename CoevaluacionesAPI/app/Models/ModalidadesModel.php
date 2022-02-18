2a4b686a36bb136596d94cc93f07e8bff902917e
chino0rt24 <67172144+chino0rt24@users.noreply.github.com>
2022-02-18 00:54:58 -0600

    Modelo Modalidad
    Modalidad Modelo

--- CoevaluacionesAPI/app/Models/ModalidadesModel.php
+++ CoevaluacionesAPI/app/Models/ModalidadesModel.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModalidadesModel extends Model
 {
-    use HasFactory;
+    protected $table="modalidades";
+    protected $hidden = [
+        
+        'Modalidad','clave','estatus'
+    ];
 }
